156cf17378dcab1c95e840980c4c1a00b80d17b1
sharlottekoren <sharlottekoren@gmail.com>
2025-12-01 13:03:02 +0000

    add initial main.go file with basic Hello World implementation

---
+++ cmd/app/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// This is just to test that the main function is working.
+// In order to run this I had to run `go mod init` and create a go.mod file.
+// A go.mod file tells Go what your project is called and which outside packages it depends on.
+func main() {
+	fmt.Println("Hello, World!")
+}
